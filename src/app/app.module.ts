331122f840881382135797205af69d2ce448c4b3
Vidhi Aggarwal <vidhiaggarwal397@gmail.com>
2022-09-02 22:26:35 +0530

    Addede Mat-icons, mat-fab, mat-mini-fab, mat-icons buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button"
+import {MatIconModule} from "@angular/material/icon"
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import {MatButtonModule} from "@angular/material/button"
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
