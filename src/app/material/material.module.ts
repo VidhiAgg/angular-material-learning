128cece3d325fdf83ebc113fd54da0c3cdc6497b
Vidhi Aggarwal <vidhiaggarwal397@gmail.com>
2022-09-03 18:18:30 +0530

    Added simple list in country

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,12 +4,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+
 
 const MateraialComponents=[
   MatButtonModule,
   MatIconModule,
   MatInputModule,
   MatFormFieldModule,
+  MatSelectModule
 
 ]
 
